5bb0a34f9cb8b8439dbf2081ab5dcd27e8e296b8
Renan Siqueira <renan.siqueira8a@gmail.com>
2024-11-27 16:46:17 -0300

    Add features - colored and board size
    Add colored checkbox feature and board size selection.
    Add mouse events. Ctrl to draw, shift to erase, c to clear.

--- scripts.js
+++ scripts.js
@@ -1,60 +1,79 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#btn-set-size");
+const boardSizeSelect = document.querySelector('#board-size');
 const h1 = document.querySelector("h1");
-const containerWidth = 580;
-let shiftPressed = false;
+const checkboxColored = document.querySelector('#colored');
+let containerWidth = boardSizeSelect.value;
+let squareSize = 16;
 
 container.style.width = containerWidth+'px';
 
+boardSizeSelect.addEventListener("change", (e) => {
+    containerWidth = e.target.value;
+    container.style.width = containerWidth+'px';
+    updateContainerGrid(containerWidth);
+})
+
 btn.addEventListener("click", () => {
-    let userSize = 0;
     while(true){
-        userSize = Number(prompt('Enter an integer number from 0 to 100.'));
-        console.log(`userSize = ${userSize}`);
-        console.log(`userSize type = ${typeof userSize}`);
-        console.log(typeof userSize === 'number');
-        console.log(userSize === Math.floor(userSize));
-        console.log(userSize !== NaN);
-        if(typeof userSize === 'number' && userSize === Math.floor(userSize) &&
-            userSize !== NaN && userSize <= 100){
-            h1.textContent = `Grid size: ${userSize}x${userSize}`
-            createGrid(userSize);
+        squareSize = Number(prompt('Enter an integer number from 0 to 100.'));
+        if(typeof squareSize === 'number' && squareSize === Math.floor(squareSize) &&
+            !isNaN(squareSize) && squareSize <= 100){
+            h1.textContent = `${squareSize}x${squareSize}`
+            createGrid(squareSize);
             break;
         } else {
             alert('Not a valid number! Try again!');
         }
     }
 });
-document.addEventListener("keydown", (e) => {
-    if(e.shiftKey){
-        shiftPressed = true;
+document.addEventListener("keypress", (e) => {
+    if(e.code === 'KeyC'){
+        createGrid(squareSize);
     }
 });
-document.addEventListener("keyup", (e) => {
-    shiftPressed = false;
-});
-/* document.addEventListener("keypress", (e) => {
-    if(e.cKey){
-        createGrid();
-    }
-}); */
 container.addEventListener("mouseover", (e) => {
-    if(shiftPressed){
-        let target = e.target;
-        target.style.backgroundColor = 'black';
+    let target = e.target;
+    if(e.ctrlKey && e.target.classList.contains('square')){
+        if(checkboxColored.checked){
+            target.style.backgroundColor = getRandomRGBA();
+        } else {
+            target.style.backgroundColor = 'black';
+        }
+    } else if(e.shiftKey && e.target.classList.contains('square')){
+        target.style.backgroundColor = 'transparent';
     }
 });
 
-function createGrid(userSize){
+function createGrid(squareSize){
     container.textContent = '';
-    for (let i = 0; i < userSize*userSize; i++) {
+    for (let i = 0; i < squareSize*squareSize; i++) {
         const square = document.createElement("div");
-        square.style.width = `${containerWidth/userSize}px`;
-        square.style.height = `${containerWidth/userSize}px`;
+        square.classList.add('square');
+        square.style.width = `${containerWidth/squareSize}px`;
+        square.style.height = `${containerWidth/squareSize}px`;
         square.style.border = '1px solid #e9e9e9';
 
         container.appendChild(square);
     }
 }
 
-createGrid(16);
+function updateContainerGrid(containerSize){
+    const containerChildren = container.children;
+    for (let i = 0; i < containerChildren.length; i++) {
+        const square = containerChildren[i];
+        square.style.width = `${containerSize/squareSize}px`;
+        square.style.height = `${containerSize/squareSize}px`;
+    }
+}
+
+function getRandomRGBA() {
+    //return Math.random() * (max - min) + min;
+    const round = Math.round;
+    const mRandom = Math.random;
+    return 'rgba(' + round(mRandom()*255) + 
+                ',' + round(mRandom()*255) + 
+                ',' + round(mRandom()*255) + ',' + '1)';
+}
+
+createGrid(squareSize);
